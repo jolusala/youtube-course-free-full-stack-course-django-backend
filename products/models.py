31640a651ed199dc47a54c240eb3fbbd97b31296
Jose Luis Salazar <jose.luissalacast@gmail.com>
2022-11-16 17:26:24 -0500

    step-3
    add products, product category and maker to products app

---
+++ products/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.postgres import fields as PostGresFields
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=256)
+    icon_url = models.URLField(blank=True)
+    description = models.TextField()
+    parent_category = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        related_name="children_categories",
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Maker(models.Model):
+    name = models.CharField(max_length=512)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    class Currency(models.TextChoices):
+        SWEDISH_CROWN = ("SEK", _("Swedish Crown"))
+        AMERICAN_DOLLAR = ("USD",_("American Dollar"))
+        EURO = ("EUR",_("Euro"))
+        POUND_STERLING = ("GBP",_("Pound Sterling"))
+        YEN = ("JPY",_("Yen"))
+        AUSTRALIAN_DOLLAR = ("AUD",_("Australian Dollar"))
+
+    title = models.CharField(max_length=512)
+    subtitle = models.CharField(max_length=512)
+
+    maker = models.ForeignKey(
+        Maker,
+        on_delete=models.CASCADE,
+        related_name="products",
+        blank=True,
+        null=True,
+    )
+# Create your models here.
+
+    image1_url = models.URLField(blank=True, null=True)
+    image1_ur2 = models.URLField(blank=True, null=True)
+    image1_ur3 = models.URLField(blank=True, null=True)
+    image1_ur4 = models.URLField(blank=True, null=True)
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    currrency = models.CharField(
+        max_length=3,
+        choices=Currency.choices,
+        default=Currency.AMERICAN_DOLLAR,
+
+    )
+
+    variation_products_ids = PostGresFields.ArrayField(
+        models.IntegerField(null=True,blank=True),
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.subtitle} - {self.maker}"
