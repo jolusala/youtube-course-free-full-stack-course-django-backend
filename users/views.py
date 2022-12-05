02e82da2f2e9792f4203e309229b99f76a0ba777
Jose Luis Salazar <jose.luissalacast@gmail.com>
2022-12-05 16:24:49 -0500

    step-7
    add /users/register/ endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from users.serializers import CustomUserRegisterSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class CustomUserRegisterView(generics.CreateAPIView):
+    model = get_user_model()
+    serializer_class = CustomUserRegisterSerializer
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        return Response(status=status.HTTP_201_CREATED)
